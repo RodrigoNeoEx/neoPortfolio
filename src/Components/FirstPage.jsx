a0e3629a67dcc19cb5208d8fa78fb674e0025bef
NeoEx <draja.acc@gmail.com>
2024-11-22 11:25:06 -0300

    voltando a codar e fazendo testes

--- src/Components/FirstPage.jsx
+++ src/Components/FirstPage.jsx
@@ -14,7 +14,10 @@ render() {
       <div className="cargo">FrontEnd Web Developer</div>
       <Calendar />
       <div className="description"></div>
-      <p className="text">Completely in love with effects, design and all dynamics related to the front end. <br></br>This is me!<br></br>Currently studying Full Stack Web Development at Trybe and looking for new challenges/jobs in the WebDev market</p>
+      <p className="text">Completely in love with effects, design and all dynamics related to the front end. <br>
+        </br>This is me!<br>
+        </br>Currently studying Full Stack Web Development at Trybe and looking for new challenges/jobs in the WebDev market
+      </p>
     </div>
   </div>
   )};
